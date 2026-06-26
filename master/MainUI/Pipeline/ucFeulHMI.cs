49e684ac5e1da38baaa0fb42a9bb14178b9808e3
p_ym <2570874156@qq.com>
2026-06-26 18:51:01 +0800

    计算燃油油耗率，发动机功率为0时改用电功率计算

--- master/MainUI/Pipeline/ucFeulHMI.cs
+++ master/MainUI/Pipeline/ucFeulHMI.cs
@@ -36,6 +36,9 @@ namespace MainUI
         // 模拟量的集合
         Dictionary<string, ucPipePara> DoubleDicValve = new Dictionary<string, ucPipePara>();
 
+        //创建一个三项数据对象
+        ThreePhaseElectric threePhaseElectric = new ThreePhaseElectric();
+
         // 测试GIT
         public void Init()
         {
@@ -479,7 +482,18 @@ namespace MainUI
             var MassFlowCC = this.ucPipePara14.Value - this.ucPipePara9.Value;
 
             this.flowDiff.Text = Math.Round(MassFlowCC, 1).ToString();
-            this.lblOilCoast.Text = Math.Round(MassFlowCC *1000 / MiddleData.instnce.EnginePower, 1).ToString();
+
+            //计算燃油油耗率
+            //因为重量断开连接之后，值无穷带，所以当发动机功率为零的时候换电功率计算
+            if (MiddleData.instnce.EnginePower == 0)
+            {
+                this.lblOilCoast.Text = Math.Round(MassFlowCC * 1000 / threePhaseElectric.DataValue["有功功率"], 1).ToString();
+            }
+            else
+            { 
+                this.lblOilCoast.Text = Math.Round(MassFlowCC *1000 / MiddleData.instnce.EnginePower, 1).ToString();
+            }
+            
         }
 
         private void switchPictureBox13_SwitchChanged(object sender, bool value)
